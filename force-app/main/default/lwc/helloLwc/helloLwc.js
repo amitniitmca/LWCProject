4ce392decca2d5b5615061740ac97fd6d5299f99
Amit Kumar <amitniitmca@gmail.com>
2023-03-31 07:32:02 +0530

    First LWC

--- force-app/main/default/lwc/helloLwc/helloLwc.js
+++ force-app/main/default/lwc/helloLwc/helloLwc.js
@@ -1,3 +1,19 @@
 import { LightningElement } from 'lwc';
 
-export default class HelloLwc extends LightningElement {}
+export default class HelloLwc extends LightningElement {
+
+    textValue;
+    userName;
+
+    handleTextChange(event){
+        this.textValue = event.detail.value;
+    }
+
+    handleSubmitClick(event){
+        this.userName = this.textValue;
+    }
+}
+
+
+// event.detail
+// event.target
